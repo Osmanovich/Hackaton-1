e7bf0e6193aa430e242f5799204e26c602868499
Osmanovich <osmanovicahmed1@gmail.com>
2022-06-09 22:18:12 +0200

    change title

--- src/components/Heading/index.js
+++ src/components/Heading/index.js
@@ -3,7 +3,7 @@ import { register } from "@frukmruk/html";
 const Heading = () => {
   return (
     <div id="heading" class="mb-6 p-2 h-16 md:w-full text-2xl">
-      #Heading
+      #Tilia Rentals
     </div>
   );
 };
